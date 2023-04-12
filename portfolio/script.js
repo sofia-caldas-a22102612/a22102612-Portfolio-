d62c37e3d84319d9364fd4eb502dc71a5a9ab20a
Sofia <sofiacaldas1999@gmail.com>
2023-04-12 18:02:04 +0100

    hello

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var element = document.body;
+    var image = document.getElementById("bimba");
+
+    image.innerHTML = "<i class=\"fa-solid fa-fish fa-xl\" style=\"color: #000000;\"></i>";
+    element.classList.toggle("dark-mode");
+
+}
+        
