2bbf17e5f3b9c3231975e331d340e59964dc6b60
Sofia <sofiacaldas1999@gmail.com>
2023-04-25 16:03:57 +0100

    dwefw

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 
 function myFunction() {
-  var menu = document.getElementById("dropbtn");
+ 
   var image = document.getElementById("bimba");
-  var main = document.getElementById("main");
   var element = document.body;
+  
   element.classList.toggle("dark-mode");
 
+
   if (image.innerHTML == "<i class=\"fa-solid fa-fish fa-xl\" style=\"color: #000000;\"></i>") {
-    menu.innerHTML = "<button id = \"dropbtn\" class=\"dropbtn\">Menu</button>";
     image.innerHTML = "<i class=\"fa-regular fa-flag\" style=\"color: #000000;\"></i>";
-    document.getElementById("main").className = "main";
   }
   else {
-    menu.innerHTML = "<button id = \"dropbtn\" class=\"dropbtnDark\">Menu</button>";
     image.innerHTML = "<i class=\"fa-solid fa-fish fa-xl\" style=\"color: #000000;\"></i>";
-    document.getElementById("main").className = "mainDarkMode";
   }
 }
 
