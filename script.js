0830111548c0c9b4f9232a26ddfa521543ffe911
Sofia <sofiacaldas1999@gmail.com>
2023-04-26 16:19:57 +0100

    asd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function playClickSound() {
-  var audio = new Audio('sounds\interface-soft-abbreviated-click-131438.mp3');
-  audio.play();
+  var audio = document.getElementById("audio");
+
 }
 
 
@@ -8,15 +8,20 @@ function myFunction() {
  
   var image = document.getElementById("bimba");
   var element = document.body;
+  var article = document.getElementById("article");
+  var audio = document.getElementById("audio");
   
   element.classList.toggle("dark-mode");
+  article.classList.toggle("dark-mode");
 
 
   if (image.innerHTML == "<i class=\"fa-solid fa-fish fa-xl\" style=\"color: #000000;\"></i>") {
     image.innerHTML = "<i class=\"fa-regular fa-flag\" style=\"color: #000000;\"></i>";
+    audio.innerHTML = "  <audio controls src=\"sounds\sadness-in-roads-to-nowhere-23407.mp3\" autoplay=\"true\" loop=\"true\"></audio>";
   }
   else {
     image.innerHTML = "<i class=\"fa-solid fa-fish fa-xl\" style=\"color: #000000;\"></i>";
+    audio.innerHTML = "<audio controls src=\"sounds\galaxy-140898.mp3\" autoplay=\"true\" loop=\"true\"></audio>";
   }
 }
 
